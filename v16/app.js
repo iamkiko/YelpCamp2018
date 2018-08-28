c2ea1e557a6e0a6c3530e8f50c51b7ad8b47904b
Christos Constantinides <constantinides.ch@gmail.com>
2018-08-28 12:14:58 +0000

    v16 - added moment.js for time stamp of submissions

---
+++ v16/app.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+var     express = require("express"),
+        app = express(),
+        bodyParser = require("body-parser"),
+        mongoose = require("mongoose"),
+        flash = require("connect-flash"),
+        passport = require("passport"),
+        LocalStrategy = require("passport-local"),
+        methodOverride = require("method-override"),
+        Campground = require("./models/campground"),
+        Comment = require("./models/comment"),
+        User = require("./models/user"),
+        seedDB = require("./seeds");
+
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index");
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp_v13', { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); // executes seedDB from module exports in seeds.js -> removes campgrounds
+
+app.locals.moment = require('moment');
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//middleware to add currentUser to every route and template
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error"); //adding the flash message
+    res.locals.success = req.flash("success"); //adding the flash message
+    next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("We have liftoff!");
+});
